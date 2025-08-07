06a2a1a1dc3b542700adf76d416c45cf3374cf9f
Moji <najimoh170@gmail.com>
2025-08-06 22:22:05 -0700

    Initial commit of LumaScan project

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,20 @@
+// src/App.tsx
+
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div style={{
+      display: "flex",
+      height: "100vh",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "3rem",
+      fontWeight: "bold"
+    }}>
+      LumaScan Frontend is running 
+    </div>
+  );
+};
+
+export default App;
